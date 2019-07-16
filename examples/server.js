ef85b34550f284d01efbcb2ac7c9e965f1ca9779
jiaguo <646293003@qq.com>
2019-07-16 22:46:35 +0800

    feat:

--- examples/server.js
+++ examples/server.js
@@ -24,15 +24,43 @@ router.get('/simple/get', function(req, res) {
   })
 })
 
-app.use(router)
-app.use(webpackHotMiddleware(compiler))
+router.get('/base/get', function(req, res) {
+    res.json(req.query)
+  })
 
-app.use(express.static(__dirname))
+router.post('/base/post', function(req, res) {
+  res.json(req.body)
+//   res.json({
+//     msg: req.body||'ssss'
+//   })
+})
+
+router.post('/base/buffer', function(req, res) {
+  let msg = []
+  req.on('data', (chunk) => {
+    if (chunk) {
+      msg.push(chunk)
+    }
+  })
+  req.on('end', () => {
+    let buf = Buffer.concat(msg)
+    res.json(buf.toJSON())
+  })
+})
+
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(router)
+
+app.use(webpackHotMiddleware(compiler))
+
+
+app.use(express.static(__dirname))
 
 const port = process.env.PORT || 8080
 module.exports = app.listen(port, () => {
   console.log(`Server listening on http://localhost:${port}, Ctrl+C to stop`)
 })
+
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,29 @@
-import { AxiosRequestConfig } from './types'
+import { AxiosRequestConfig, AxiosPromise } from './types'
 import xhr from './xhr'
+import { bulidURL } from './helpers/url'
+import { transformRequest } from './helpers/data'
+import { processHeaders } from './helpers/headers'
 
-function axios(config: AxiosRequestConfig):void {
-    xhr(config)
+function axios(config: AxiosRequestConfig): AxiosPromise {
+  processConfig(config)
+  return xhr(config)
+}
+function processConfig(config: AxiosRequestConfig): void {
+  config.url = transformURL(config)
+  config.headers = transformHeaders(config)
+  config.data = transformRequestData(config)
+}
+function transformURL(config: AxiosRequestConfig): string {
+  const { url, params } = config
+  return bulidURL(url, params)
+}
+function transformRequestData(config: AxiosRequestConfig): any {
+  return transformRequest(config.data)
+}
+
+function transformHeaders(config: AxiosRequestConfig) {
+  const { headers = {}, data } = config
+  return processHeaders(headers, data)
 }
 
 export default axios
