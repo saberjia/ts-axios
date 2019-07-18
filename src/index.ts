e1af19eee15e37e6e7b41aa3b98f8cbad088adac
jiaguo <646293003@qq.com>
2019-07-18 22:11:55 +0800

    feat:

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,5 @@
-import { AxiosRequestConfig, AxiosPromise } from './types'
-import xhr from './xhr'
-import { bulidURL } from './helpers/url'
-import { transformRequest } from './helpers/data'
-import { processHeaders } from './helpers/headers'
+import axios from './axios'
 
-function axios(config: AxiosRequestConfig): AxiosPromise {
-  processConfig(config)
-  return xhr(config)
-}
-function processConfig(config: AxiosRequestConfig): void {
-  config.url = transformURL(config)
-  config.headers = transformHeaders(config)
-  config.data = transformRequestData(config)
-}
-function transformURL(config: AxiosRequestConfig): string {
-  const { url, params } = config
-  return bulidURL(url, params)
-}
-function transformRequestData(config: AxiosRequestConfig): any {
-  return transformRequest(config.data)
-}
-
-function transformHeaders(config: AxiosRequestConfig) {
-  const { headers = {}, data } = config
-  return processHeaders(headers, data)
-}
+export * from './types'
 
 export default axios
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,4 @@
-//公用的类型定义文件
+// 公用的类型定义文件
 
 export type Method =
   | 'get'
@@ -25,19 +25,83 @@ export type XMLHttpRequestResponseType =
   | 'text'
 
 export interface AxiosRequestConfig {
-  url: string
+  url?: string
   method?: Method
   data?: any
   params?: any
   headers?: any
   responseType?: XMLHttpRequestResponseType
+  timeout?: number
 }
-export interface AxiosResponse {
-  data: any
+export interface AxiosError extends Error {
+  config: AxiosRequestConfig
+  code?: string
+  request?: any
+  response?: AxiosResponse
+  isAxiosError: boolean
+}
+
+export interface AxiosResponse<T = any> {
+  data: T
   status: number
   statusText: string
   headers: any
   config: AxiosRequestConfig
   request: any
 }
-export interface AxiosPromise extends Promise<AxiosResponse> {}
+
+export interface AxiosPromise<T = any> extends Promise<AxiosResponse<T>> {}
+
+export interface Axios {
+  interceptors: {
+    request: AxiosInterceptorManager<AxiosRequestConfig>
+    response: AxiosInterceptorManager<AxiosResponse>
+  }
+  request<T = any>(config: AxiosRequestConfig): AxiosPromise<T>
+
+  get<T = any>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>
+
+  delete<T = any>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>
+
+  head<T = any>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>
+
+  options<T = any>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>
+
+  post<T = any>(
+    url: string,
+    data?: any,
+    config?: AxiosRequestConfig
+  ): AxiosPromise<T>
+
+  put<T = any>(
+    url: string,
+    data?: any,
+    config?: AxiosRequestConfig
+  ): AxiosPromise<T>
+
+  patch<T = any>(
+    url: string,
+    data?: any,
+    config?: AxiosRequestConfig
+  ): AxiosPromise<T>
+}
+
+export interface AxiosInstance extends Axios {
+  <T = any>(config: AxiosRequestConfig): AxiosPromise<T>
+
+  <T = any>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>
+}
+
+export interface AxiosInterceptorManager<T> {
+  use(resolved: ResolvedFn<T>, rejected?: RejectedFn): number
+
+  eject(id: number): void
+}
+
+export interface ResolvedFn<T = any> {
+  (val: T): T | Promise<T>
+}
+
+export interface RejectedFn {
+  (error: any): any
+}
